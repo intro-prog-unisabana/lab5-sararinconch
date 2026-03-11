9bd029c9164846d4c9a1f570fe38b37730a191b0
Sara Rincón <sararich@unisabana.edu.co>
2026-03-11 15:07:11 -0500

    Problema 1.2

--- functions.py
+++ functions.py
@@ -1,12 +1,11 @@
 
 notas = [85, 92, 78]
 def promedio_estudiante(notas):
+    if notas == []:
+        return float()
     Promedio = sum(notas)/len(notas)
     return float(Promedio)
 print(promedio_estudiante(notas))
-
-
-
 
 
 
