71b6f696ff04d15fb1f426fd4acc64597eb33a1d
Sara Rincón <sararich@unisabana.edu.co>
2026-03-15 05:15:06 -0500

    Problema 11

--- debug.py
+++ debug.py
@@ -1,19 +1,20 @@
-def find_max(a, b):
-  max_number = a
+def find_max(a, b, c):
   if a > b and a > c:
     max_number = a
   elif b > a and b > c:
     max_number = b
   else:
     max_number = c
+  return max_number
 
 #FREEZE CODE BEGIN
-x = int(input("Enter a number:\n"))
-y = int(input("Enter a number:\n"))
-z = int(input("Enter a number:\n"))
+a = int(input("Enter a number:\n"))
+b = int(input("Enter a number:\n"))
+c = int(input("Enter a number:\n"))
 #FREEZE CODE END
 
-print("Maximum value:", maximum)
+max_number = find_max(a, b, c)
+print("Maximum value:", max_number)
 
 
 
