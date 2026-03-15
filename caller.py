e9bcbc4baaae5f97584e566b75ac195253179360
Sara Rincón <sararich@unisabana.edu.co>
2026-03-15 05:42:09 -0500

    Problema 13

--- caller.py
+++ caller.py
@@ -1,8 +1,12 @@
-
-
 # FREEZE CODE BEGIN
 x = int(input())
 y = float(input())
 # FREEZE CODE END
+
+z = "quiz_test"
+
+from mystery_module import transform_data
+resultado = transform_data(x, y, z)
+print(resultado)
 
 
